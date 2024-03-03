e36070f0aa0d17f8ab35bd5e78751c4315ccba7b
Dudus Jestem <dudak799@gmail.com>
2024-03-03 19:55:26 +0100

    added makefile, begun adding plugins

---
+++ data/configuration/configuration.go
@@ -0,0 +1,72 @@
+package configuration
+
+import (
+	"network/data/errors"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
+
+type DatabaseConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     uint   `mapstructure:"port"`
+}
+
+type BridgeConfig struct {
+	BrokerHost            string `mapstructure:"broker_url"`
+	BrokerPort            uint   `mapstructure:"broker_port"`
+	ClientId              string `mapstructure:"client_id"`
+	KeepAliveSeconds      uint   `mapstructure:"keep_alive_seconds"`
+	DisconnectMiliseconds uint   `mapstructure:"disconnect_miliseconds"`
+}
+
+type APIConfig struct {
+	Port               uint `mapstructure:"port"`
+	JWTLifespanMinutes uint `mapstructure:"jwt_lifespan_minutes"`
+}
+
+type GlobalConfig struct {
+	Database DatabaseConfig `mapstructure:"database"`
+	Bridge   BridgeConfig   `mapstructure:"bridge"`
+	API      APIConfig      `mapstructure:"api"`
+}
+
+
+var Config GlobalConfig
+
+func loadEnv() error {
+	err := godotenv.Load()
+    if err != nil {
+        return errors.ErrReadingEnvFile
+    }
+
+	return nil
+}
+
+func loadConfig(config *GlobalConfig) error {
+	viper.SetConfigFile("config.toml")
+	viper.SetConfigType("toml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return errors.ErrReadingConfigFile
+	}
+
+	err := viper.Unmarshal(config)
+	if err != nil {
+		return errors.ErrFormattingConfigFile
+	}
+
+	return nil
+}
+
+func Initialize() (*GlobalConfig, error) {
+	if err := loadConfig(&Config); err != nil {
+		return nil, err
+	}
+	
+	if err := loadEnv(); err != nil {
+		return nil, err
+	}
+
+	return &Config, nil
+}
---
+++ makefile
@@ -0,0 +1,29 @@
+# Makefile
+
+BINARY := network
+
+BUILDDIR := ./build
+PLUGINDIR := ./plugins
+
+GO := go
+GOFLAGS := -v
+
+PLUGINTAG := plugin
+PLUGINMODE := plugin
+PLUGINPATTERN := $(wildcard $(PLUGINDIR)/*)
+PLUGINBUILD := $(patsubst $(PLUGINDIR)/%,$(BUILDDIR)/plugins/%.so,$(PLUGINPATTERN))
+
+.PHONY: all clean
+
+all: $(BUILDDIR)/$(BINARY) plugins
+
+$(BUILDDIR)/$(BINARY):
+	$(GO) build $(GOFLAGS) -o $(BUILDDIR)/$(BINARY)
+
+plugins: $(PLUGINBUILD)
+
+$(BUILDDIR)/plugins/%.so: $(PLUGINDIR)/%
+	$(GO) build $(GOFLAGS) -tags $(PLUGINTAG) -buildmode=$(PLUGINMODE) -o $@ $</main.go
+
+clean:
+	$(RM) $(BUILDDIR)/$(BINARY) $(PLUGINBUILD)
